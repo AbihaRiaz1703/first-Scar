48eacc34c6da64e0c76f55daf272957cef1545e1
AbihaRiaz1703 <RamshaRiaz039@gmail.com>
2024-10-30 20:46:32 +0500

    test comment

--- lib/same.dart
+++ lib/same.dart
@@ -10,6 +10,56 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 137, 181, 218),
+      resizeToAvoidBottomInset: true,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('images/street.jpg'),
+                height: 60,
+                width: 60,
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Miss Falafel',
+                    style: TextStyle(fontFamily: 'HomemadeApple', fontSize: 30),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Column(
+            children: [
+              Text(
+                'Login',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ],
+      )),
+    );
   }
 }
